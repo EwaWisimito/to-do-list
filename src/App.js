d921158e4ab65160e2ba06559267f3e4c7e84374
EwaWisimito <awemaj@gmail.com>
2019-02-09 11:23:11 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import TaskContainer from './TaskContainer';
 
 class App extends Component {
+
+  state ={
+    tasks: [
+      {id:1, name: 'odkurzyć', completed: false},
+      {id:2, name: 'zrobić zakupy', completed: true},
+      {id:3, name: 'Nauczyć się Reacta', completed: false},
+  ]
+  };
+  handleChange = (event, taskId) => {
+    // console.log('ngnvf', taskId);
+    // console.log(event.target.checked)
+
+    const taskIndex = this.state.tasks.findIndex(task => task.id === taskId);
+    const task = {...this.state.tasks[taskIndex]};
+
+    task.completed = event.target.checked
+
+    const tasks = [...this.state.tasks]
+    tasks[taskIndex]=task;
+
+    this.setState({tasks: tasks});
+   }
+
   render() {
     return (
       <div className="App">
-      <TaskContainer />
+      <TaskContainer
+       todos={this.state.tasks} 
+       handleChange = {this.handleChange}
+       />
        
       </div>
     );
--- src/TaskContainer.js
+++ src/TaskContainer.js
@@ -4,38 +4,15 @@ import Task from './Task';
 
 
 class TaskContainer extends Component {
-  state ={
-    tasks: [
-      {id:1, name: 'odkurzyć', completed: false},
-      {id:2, name: 'zrobić zakupy', completed: true},
-      {id:3, name: 'Nauczyć się Reacta', completed: false},
-  ]
-  };
-  handleChange = (event, taskId) => {
-    // console.log('ngnvf', taskId);
-    // console.log(event.target.checked)
-
-    const taskIndex = this.state.tasks.findIndex(task => task.id === taskId);
-    const task = {...this.state.tasks[taskIndex]};
-
-    task.completed = event.target.checked
-
-    const tasks = [...this.state.tasks]
-    tasks[taskIndex]=task;
-
-    this.setState({tasks: tasks});
-   }
-
-
 
     render() {
       return (
         <div className="task-container">
-        {this.state.tasks.map(task =>(
+        {this.props.todos.map(task =>(
           <Task
           key={`task-${task.id}`}
           task={task}
-          handleChange={(event)=>this.handleChange(event, task.id)}
+          handleChange={(event)=>this.props.handleChange(event, task.id)}
           />
           ))}
         </div>
